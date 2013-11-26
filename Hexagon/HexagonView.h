5e5e632bd9689b6018b6801acb48cd119f4855b6
Ben Chatelain <ben.d.chatelain@kp.org>
2013-11-26 16:12:17 -0700

    Create HexagonView

---
+++ Hexagon/HexagonView.h
@@ -0,0 +1,13 @@
+//
+//  HexagonView.h
+//  Hexagon
+//
+//  Created by Ben Chatelain (KP) on 11/26/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HexagonView : UIView
+
+@end
---
+++ Hexagon/HexagonView.m
@@ -0,0 +1,53 @@
+//
+//  HexagonView.m
+//  Hexagon
+//
+//  Created by Ben Chatelain (KP) on 11/26/13.
+//
+//
+
+#import "HexagonView.h"
+
+@implementation HexagonView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame])) {
+        [self configureLayerForHexagon];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder
+{
+    if ((self = [super initWithCoder:coder])) {
+        [self configureLayerForHexagon];
+    }
+    return self;
+}
+
+- (void)configureLayerForHexagon
+{
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    maskLayer.fillRule = kCAFillRuleEvenOdd;
+    maskLayer.frame = self.bounds;
+
+    CGFloat width = self.frame.size.width;
+    CGFloat height = self.frame.size.height;
+    CGFloat hPadding = width * 1 / 8 / 2;
+
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(width/2, 0)];
+    [path addLineToPoint:CGPointMake(width - hPadding, height / 4)];
+    [path addLineToPoint:CGPointMake(width - hPadding, height * 3 / 4)];
+    [path addLineToPoint:CGPointMake(width / 2, height)];
+    [path addLineToPoint:CGPointMake(hPadding, height * 3 / 4)];
+    [path addLineToPoint:CGPointMake(hPadding, height / 4)];
+    [path closePath];
+    [path fill];
+    maskLayer.path = path.CGPath;
+
+    self.layer.mask = maskLayer;
+}
+
+@end
